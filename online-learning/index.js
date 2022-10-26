e1276f206a6d9ce226185fb9cd776d37a6139976
anhuet <volocanh102@gmail.com>
2022-10-26 16:56:38 +0700

    covert online learning figma to html using tailwind

---
+++ online-learning/index.js
@@ -0,0 +1,22 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        yellow: "#F48C06",
+        blue: "#65DAFF",
+        darkBlue: "2F327D",
+        hero: "#FFF2E1",
+        grayCustom: "#464646",
+        textBlue: "#252641",
+        grayText: "#696984",
+      },
+    },
+  },
+};
+
+// const navbar = document.getElementById("navbar-default");
+// const menuToggle = document.getElementById("menu-toggle");
+// menuToggle.addEventListener("click", () => {
+//   console.log(menuToggle);
+//   navbar.style.display = "block";
+// });
